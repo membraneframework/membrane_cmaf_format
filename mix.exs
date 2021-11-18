e06191efa7004e47b8c670652bb08c80b13dd3c9
AndrzejPodo <andrzejpodobinski2@gmail.com>
2021-11-18 10:18:59 +0100

    bump to 0.4.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.CMAF.MixProject do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.4.0"
   @github_url "https://github.com/membraneframework/membrane_cmaf_format"
 
   def project do
@@ -49,7 +49,7 @@ defmodule Membrane.CMAF.MixProject do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.7.0"},
+      {:membrane_core, "~> 0.8.0"},
       {:ex_doc, "~> 0.24.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1.0", only: [:dev, :test], runtime: false}
     ]
