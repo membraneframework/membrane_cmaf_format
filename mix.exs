a08d364f35013571a24dd2729cd32abaa6eb0f3d
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2022-01-10 10:51:54 +0100

    Bump version to 0.5.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.CMAF.MixProject do
   use Mix.Project
 
-  @version "0.4.0"
+  @version "0.5.0"
   @github_url "https://github.com/membraneframework/membrane_cmaf_format"
 
   def project do
